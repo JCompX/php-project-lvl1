3fc0df8be53feb7abef802ed569ad3f2931149df
JCompX <freelansermail@gmail.com>
2021-07-08 17:59:48 +0300

    refactoring stage #5

---
+++ src/Engine.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Brain\Games\Engine;
+
+use function cli\line;
+use function cli\prompt;
+use function Brain\Games\Cli\startGame;
+
+
+const TASK_COUNTER = 3;
+
+function taskFlow($descriptionTask, $arr)
+{
+    
+    $name = startGame();
+    line($descriptionTask);
+    foreach ($arr as [$showedQuestion, $trueAnswer]) {
+        $userAnswer = prompt("Question: " . $showedQuestion);
+        if ($trueAnswer === $userAnswer) {
+            line("Your answer: {$userAnswer}");
+            line('Correct!');
+        } else {
+            line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$trueAnswer}'");
+            line("Let's try again, {$name}!");
+            return;
+        }
+    }
+    line("Congratulations, {$name}!");
+}
---
+++ src/Games/Even.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Brain\Games\Even;
+
+use function Brain\Games\Engine\taskFlow;
+
+use const Brain\Games\Engine\TASK_COUNTER;
+
+function brainEven()
+{
+    $descriptionTask = 'Answer "yes" if the number is even, otherwise answer "no".';
+    $arr = [[]];    
+    taskFlow($descriptionTask, checkForTrue($arr));
+}
+
+function checkForTrue($arr) 
+{
+    for ($i = 0; $i < TASK_COUNTER; $i++) {
+        $arr[$i][0] = rand(1, 1000);
+        $arr[$i][1] = ($arr[$i][0] % 2 === 0) ? 'yes' : 'no';
+    }
+    return $arr;
+}
