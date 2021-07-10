af181efe1cf0bbba0854dd40b6cab0ea146192ba
JCompX <freelansermail@gmail.com>
2021-07-10 20:16:21 +0300

    Games of Mind, stage #9, done

---
+++ src/Games/Prime.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Brain\Games\Prime;
+
+use function Brain\Games\Engine\taskFlow;
+
+function prime()
+{
+    $descriptionTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    taskFlow($descriptionTask, genPairs());
+}
+
+function genPairs()
+{
+    $arrValues = [[]];
+    for ($i = 0; $i < 3; $i++) {
+        $d = rand(2, 500);
+        $flag = primeCheck($d);
+        if ($flag == 1) {
+            $trueAnswer = 'yes';
+        } else {
+            $trueAnswer = 'no';
+        }
+        $arrValues[$i][0] = $d;
+        $arrValues[$i][1] = $trueAnswer;
+    }
+    return $arrValues;
+}
+
+function primeCheck($number)
+{
+    if ($number == 1)
+        return 0;
+    for ($i = 2; $i <= $number/2; $i++){
+        if ($number % $i == 0)
+            return 0;
+    }
+    return 1;
+}
