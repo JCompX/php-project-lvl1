016168b89df5006c7e1e9c570861cf654f2a02e3
JCompX <freelansermail@gmail.com>
2021-07-10 20:18:50 +0300

    Stage #9, phpcs checked for Prime.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -29,11 +29,13 @@ function genPairs()
 
 function primeCheck($number)
 {
-    if ($number == 1)
+    if ($number == 1) {
         return 0;
-    for ($i = 2; $i <= $number/2; $i++){
-        if ($number % $i == 0)
+    }
+    for ($i = 2; $i <= $number / 2; $i++) {
+        if ($number % $i == 0) {
             return 0;
+        }
     }
     return 1;
 }
